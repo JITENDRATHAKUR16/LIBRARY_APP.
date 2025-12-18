5f8a9d4e5df06512d9989e8b30641d3a688bc267
JITENDRATHAKUR16 <jitendra161@outlook.com>
2025-12-18 21:38:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,8 @@
 import streamlit as st
 from streamlit_qr_barcode_scanner import streamlit_qr_barcode_scanner
 
-st.set_page_config(page_title="DIVINE LIBRARY", layout="centered")
-
 st.title("📚 Divine Library Scanner")
-
-# Barcode Scanner logic
 data = streamlit_qr_barcode_scanner()
 
 if data:
-    st.success(f"✅ Scanned: {data}")
-    st.write(f"Book details for {data} will appear here.")
-else:
-    st.info("Camera open karein aur barcode dikhayein.")
+    st.success(f"Scanned: {data}")
